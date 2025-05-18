b86b2512490b1a5304c04dd8ff2761ce7a703116
BrunooRamoos <ramosbruno.u@gmail.com>
2025-05-18 16:26:13 -0300

    add config v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,10 +7,5 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
   },
 })
